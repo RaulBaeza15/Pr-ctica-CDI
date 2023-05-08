37747fed4788fd4332fc8071b1c8cee38b4cfb35
Raúl Baeza Osuna <raulbaeza8@gmail.com>
2023-05-08 20:58:27 +0200

    cuenta ladrillos

--- Scripts/Eliminacion/InvisibleTrigger.cs
+++ Scripts/Eliminacion/InvisibleTrigger.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class InvisibleTrigger : MonoBehaviour
 {
+    private int cuentaLadrillos = 0;
+    public int getCuentaLadrillos()
+    {
+        return cuentaLadrillos;
+    }
+    public void sumaLadrillo()
+    {
+        cuentaLadrillos++;  
+    }
     private void OnTriggerEnter(Collider other)
     {
         // Acciones cuando otro objeto entra en el trigger invisible
         Debug.Log("Entró en el trigger invisible: " + other.name);
         GameObject collidedObject = other.gameObject;
+        MeshFilter meshFilter = collidedObject.GetComponent<MeshFilter>();
+
+        // Comprobar si la malla asociada es un cubo
+        if (meshFilter != null && meshFilter.mesh != null && meshFilter.mesh.name == "Cube Instance")
+        {
+            Debug.Log("El GameObject es un cubo.");
+            sumaLadrillo();
+        }
+        else
+        {
+            //Debug.Log("El GameObject no es un cubo.\nEs un "+ meshFilter.mesh.name);
+        }
         Destroy(collidedObject);
     }
 
